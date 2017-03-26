a2caca3c063489497a9d0d78385b8c2a955e2971
xkf15 <13051775153@163.com>
2017-03-26 12:45:47 +0800

    add new pages

--- src/components/pages/user/activity/newMessage/Notice.vue
+++ src/components/pages/user/activity/newMessage/Notice.vue
@@ -1,19 +1,25 @@
 <template lang="pug">
   #notice
     .title 发布公告
-    el-form.demo-dynamic(:model="formData", ref="formData")
-      el-form-item(:label="公告", :prop="formData.title", :rules="{required: true, message: '问题不能为空', trigger: 'blur'}")
-        el-input(v-model="formData.title")
-        el-input(v-model="formData.info")
-        el-button(type="primary", @click="submitForm('formData')") 提交
+    //- el-form.demo-dynamic(:model="dynamicValidateForm", ref="dynamicValidateForm", label-width="300px")
+    //-   el-form-item(label="公告", :prop="dynamicValidateForm.title", :rules="{required: true, message: '问题不能为空', trigger: 'blur'}")
+    //-     el-input(v-model="dynamicValidateForm.title", placeholder="请输入标题")
+    //-     el-input(v-model="dynamicValidateForm.info", placeholder="具体信息写在这里")
+    //-   el-form-item
+    //-     el-button(type="primary", @click="submitForm('dynamicValidateForm')") 提交
+    el-form(:model="dynamicValidateForm", ref="dynamicValidateForm", label-width="100px", class="demo-ruleForm")
+      el-form-item(label="年龄", prop="age", :rules="[{ required: true, message: '年龄不能为空'}]")
+        el-input(type="age", v-model="dynamicValidateForm.age")
+        el-button(type="primary" @click="submitForm('dynamicValidateForm')") 提交
 </template>
 <script>
 export default {
   data () {
     return {
-      formData: {
+      dynamicValidateForm: {
         title: '',
-        info: ''
+        info: '',
+        age: ''
       }
     }
   },
@@ -21,7 +27,7 @@ export default {
     submitForm (formName) {
       this.$refs[formName].validate((valid) => {
         if (valid) {
-          console.log(this.formData)
+          console.log(this.dynamicValidateForm)
         } else {
           console.log('error submit!!')
           return false
